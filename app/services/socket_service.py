9bf1caaeb8c1f3a86d6af24c1313f5206d9c2c95
Gerardo Belot <gbelot2003@hotmail.com>
2024-09-26 11:48:54 -0600

    :recycle:

--- app/services/socket_service.py
+++ app/services/socket_service.py
@@ -10,11 +10,11 @@ def init_socketio(app):
     @socketio.on('message')
     def handle_message(message):
         
-        print(f"Tu: {message}")
+
 
         respuesta = Actions().generar_respuesta(message)
 
-        print(f"GPT : {respuesta}")
+       
         send(respuesta, broadcast=True)
 
     return socketio
